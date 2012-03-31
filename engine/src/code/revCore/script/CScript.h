3089ab27d0fdfe87b1b4bcac7f7b43cd044c4764
technik <cjtech90@gmail.com>
2012-03-31 11:40:29 +0200

    WIP: Variant, Script and script machine

---
+++ engine/src/code/revCore/script/CScript.h
@@ -0,0 +1,38 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Revolution engine
+// Created by Carmelo J. Fernández-Agüera Tortosa (a.k.a. Technik)
+// on March 30th, 2012
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Script machine
+
+#ifndef _REV_CORE_SCRIPT_CSCRIPT_H_
+#define _REV_CORE_SCRIPT_CSCRIPT_H_
+
+namespace rev
+{
+	class CVariant;
+
+	class CScriptMachine
+	{
+	public:
+		typedef void(*TScriptFunction)(const CVariant&,CVariant&);
+		
+	public:
+		static void				init	();
+		static void				end		();
+		static CScriptMachine*	get		();
+
+		///\ Registers a function to the script machine
+		void	addFunction		(TScriptFunction _fn, const char * _name);
+		///\ Evaluates an expression
+		void	eval			(const char * _expression);
+		///\ Evaluates an expression and stores the result in _dst variant
+		void	eval			(const char * _expression, CVariant& _dst);
+
+	private:
+		CScriptMachine();
+		~CScriptMachine();
+	};
+}	// rev
+
+#endif // _REV_CORE_SCRIPT_CSCRIPT_H_
---
+++ engine/src/code/revCore/script/CVariant.h
@@ -0,0 +1,76 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Revolution engine
+// Created by Carmelo J. Fernández-Agüera Tortosa (a.k.a. Technik)
+// on March 30th, 2012
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Script variant
+
+#ifndef _REV_CORE_SCRIPT_CVARIANT_H_
+#define _REV_CORE_SCRIPT_CVARIANT_H_
+
+namespace rev
+{
+	class CVariant
+	{
+	public:
+		enum	EDataType
+		{
+			eNill = 0,
+			eInteger,
+			eReal,
+			eBool,
+			eList,
+			eDictionary
+		};
+	public:
+		// ---- Construction, copy and deletion -----
+		// Basic constructors
+		CVariant			();
+		CVariant			(const CVariant&);
+
+		// Typed constructors
+		CVariant			(int);
+		CVariant			(double);
+		CVariant			(bool);
+
+		// Assignment
+		CVariant& operator=	(const CVariant&);
+		CVariant& operator=	(int);
+		CVariant& operator=	(double);
+		CVariant& operator=	(bool);
+
+		// Destruction
+		~CVariant			();
+		
+		// Accessors
+		int			asInt		() const;
+		double		asDouble	() const;
+		bool		asBool		() const;
+
+		EDataType	type		() const;
+	public:
+		// Comparison
+		bool	operator==	(const CVariant&) const;
+		bool	operator==	(int) const;
+		bool	operator==	(double) const;
+		bool	operator==	(bool) const;
+		bool	operator!=	(const CVariant&) const;
+		bool	operator!=	(int) const;
+		bool	operator!=	(double) const;
+		bool	operator!=	(bool) const;
+
+	private:
+		union UDataT
+		{
+			int	i;		// Int
+			double d;	// Real
+			bool b;		// bool
+		};
+
+	private:
+		EDataType	mType;
+		UDataT		mData;
+	};
+}	// namespace rev
+
+#endif // _REV_CORE_SCRIPT_CVARIANT_H_
