50811020ce3d4a2100c1400426b11f4c5635f3fe
technik <cjtech90@gmail.com>
2012-05-09 20:44:28 +0200

    Added partial specialization in resource managers to support const char* _keys

--- engine/src/code/revCore/resourceManager/resourceManager.h
+++ engine/src/code/revCore/resourceManager/resourceManager.h
@@ -8,26 +8,40 @@
 #ifndef _REV_REVCORE_RESOURCEMANAGER_RESOURCEMANAGER_H_
 #define _REV_REVCORE_RESOURCEMANAGER_RESOURCEMANAGER_H_
 
+#include <revCore/string.h>
 #include <rtl/map.h>
 
 namespace rev
 {
 	template <class _resourceT, class _keyT>
-		class CResourceManager
+	class CResourceManager
 	{
 	public:
-		_resourceT * get				(_keyT ) const;			// Get resource by key (Implies ownership)
-		void		 registerResource	(_resourceT*, _keyT);	// Register a resource in the manager
-		void		 release			(_keyT );				// Release resource by key
-		void		 release			(_resourceT *);			// Release resource by pointer
+		_resourceT * get				(const _keyT& ) const;			// Get resource by key (Implies ownership)
+		void		 registerResource	(_resourceT*, const _keyT&);	// Register a resource in the manager
+		void		 release			(const _keyT& );				// Release resource by key
+		void		 release			(_resourceT *);					// Release resource by pointer
 	private:
 		rtl::map<_keyT, _resourceT*>	mResources;
+	};
+
+	// Partial specialization for const char * keys
+	template < class _resourceT >
+	class CResourceManager <_resourceT, const char*>
+	{
+	public:
+		_resourceT* get					(const char*) const;
+		void		registerResource	(_resourceT*, const char*);
+		void		release				(const char*);
+		void		release				(_resourceT*);
+	private:
+		rtl::map<rev::string,_resourceT*>	mResources;
 	};
 
 	// Inline implementation
 	//----------------------------------------------------------------------------------------------------------------------
 	template<class _resourceT, class _keyT>
-	_resourceT * CResourceManager<_resourceT, _keyT>::get(_keyT _x) const
+	_resourceT * CResourceManager<_resourceT, _keyT>::get(const _keyT& _x) const
 	{
 		typename rtl::map<_keyT,_resourceT*>::const_iterator i = mResources.find(_x);
 		if(i != mResources.end())
@@ -38,10 +52,29 @@ namespace rev
 	}
 
 	//----------------------------------------------------------------------------------------------------------------------
+	template<class _resourceT>
+	_resourceT * CResourceManager<_resourceT, const char*>::get(const char* _x) const
+	{
+		typename rtl::map<rev::string,_resourceT*>::const_iterator i = mResources.find(rev::string(_x));
+		if(i != mResources.end())
+		{
+			return i->second;
+		}
+		else return 0;
+	}
+
+	//----------------------------------------------------------------------------------------------------------------------
 	template<class _resourceT, class _keyT>
-	void CResourceManager<_resourceT,_keyT>::registerResource(_resourceT * _res, _keyT _x)
+	void CResourceManager<_resourceT,_keyT>::registerResource(_resourceT * _res, const _keyT& _x)
 	{
 		mResources[_x] = _res;
+	}
+
+	//----------------------------------------------------------------------------------------------------------------------
+	template<class _resourceT>
+	void CResourceManager<_resourceT,const char*>::registerResource(_resourceT * _res, const char* _x)
+	{
+		mResources[rev::string(_x)] = _res;
 	}
 
 	//----------------------------------------------------------------------------------------------------------------------
@@ -59,10 +92,31 @@ namespace rev
 	}
 
 	//----------------------------------------------------------------------------------------------------------------------
+	template<class _resourceT>
+	void CResourceManager<_resourceT,const char*>::release(_resourceT * _res)
+	{
+		typename rtl::map<string,_resourceT*>::iterator i = mResources.begin();
+		for(; i != mResources.end(); ++i)
+		{
+			if(i->second == _res)
+			{
+				mResources.erase(i);
+			}
+		}
+	}
+
+	//----------------------------------------------------------------------------------------------------------------------
 	template<class _resourceT, class _keyT>
-	void CResourceManager<_resourceT, _keyT>::release(_keyT _x)
+	void CResourceManager<_resourceT, _keyT>::release(const _keyT& _x)
 	{
 		mResources.erase(_x);
+	}
+
+	//----------------------------------------------------------------------------------------------------------------------
+	template<class _resourceT>
+	void CResourceManager<_resourceT, const char*>::release(const char* _x)
+	{
+		mResources.erase(string(_x));
 	}
 
 }	// namespace rev
