4f2046b5dd8f7c2e327834896da4d2e710af4927
technik <cjtech90@gmail.com>
2012-07-26 18:03:23 +0200

    Massive Clean-up

--- demos/buggy/bin/windows/video.rsc
+++
@@ -1,2 +0,0 @@
-screenPosition = [0.0, 0.0];
-screenSize = [640.0, 480.0];
--- games/gameClient/src/project/android/jni/Application.mk
+++
@@ -1,7 +0,0 @@
-################################################################################
-# Android makefile for rev gameClient
-# Created by Carmelo J. Fernández-Agüera Tortosa (a.k.a. Technik)
-# on September 10th, 2011
-################################################################################
-
-APP_STL:=gnustl_static
