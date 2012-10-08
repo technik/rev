56a93daee1af527a3ce863205fff44d3aa1fb18e
technik <cjtech90@gmail.com>
2012-10-08 11:25:02 +0200

    Makefile reorganizing. Given support for core module library compilation for arduino

---
+++ common/arduino/make/arduino-common.mk
@@ -0,0 +1,33 @@
+#-----------------------------------------------------------------------------------------------------------------------
+# Revolution SDK
+# Created by Carmelo J. Fdez-Agüera Tortosa (a.k.a. Technik)
+# On October 7th, 2012
+#-----------------------------------------------------------------------------------------------------------------------
+# Arduino common makefile
+
+# -- Global variables --
+ATMEGA_TOOLCHAIN := $(AVR_HOME)/bin
+
+# -- GNU Tools --
+AS := $(ATMEGA_TOOLCHAIN)/avr-as # Assembler
+AR := $(ATMEGA_TOOLCHAIN)/avr-ar # Archiver
+CC := $(ATMEGA_TOOLCHAIN)/avr-gcc # C Compiler
+CXX := $(ATMEGA_TOOLCHAIN)/avr-g++ # C++ Compiler
+OBJCOPY := $(ATMEGA_TOOLCHAIN)/avr-objcopy # Obj copy
+OBJDUMP := $(ATMEGA_TOOLCHAIN)/avr-objdump
+READELF := $(ATMEGA_TOOLCHAIN)/avr-readelf
+
+CXX_SRC := $(shell find $(CODE_DIR) -name *.cpp)
+CXX_OBJ := $(patsubst %.cpp, %.cpp.o, $(CXX_SRC))
+
+TEMP_FILES := $(CXX_OBJ)
+
+# -- Build variables --
+PREPROCESSOR_DEFINITIONS := -DATMEGA -DATMEGA2560
+WARNING_FLAGS := -Wall -Werror
+INCLUDE_FLAGS := -I$(CODE_DIR)
+CXX_COMPILE_FLAGS := -std=c++0x -mmcu=atmega2560 -fno-access-control -fno-enforce-eh-specs\
+ -fno-rtti -g0 -O1 -fno-exceptions -ffunction-sections -fdata-sections
+COMMON_C_FLAGS := $(PREPROCESSOR_DEFINITIONS) $(WARNING_FLAGS) $(INCLUDE_FLAGS)
+CC_FLAGS := $(COMMON_C_FLAGS)
+CXX_FLAGS := $(COMMON_C_FLAGS) $(CXX_COMPILE_FLAGS)
