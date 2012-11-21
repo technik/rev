f215ea771df12da6375b8d263d79fd0fafc2b3ff
Technik <cjtech90@gmail.com>
2012-11-21 19:08:41 +0100

    Created byte stream interfaces in revCore. Created arduinoLink project to test communications with arduino boards.

---
+++ projects/arduinoLink/prj/windows/vs12/main.cpp
@@ -0,0 +1,17 @@
+//----------------------------------------------------------------------------------------------------------------------
+// Revolution SDK
+// Created by Carmelo J. Fdez-Agüera Tortosa a.k.a. (Technik)
+// On November 21st, 2012
+//----------------------------------------------------------------------------------------------------------------------
+// Main
+
+#include <revCore/stream/byteStream/byteStream.h>
+#include <iostream>
+
+using namespace std;
+
+int main(int _argC, const char** _argV)
+{
+	cout << "Works" << endl;
+	return 0;
+}
