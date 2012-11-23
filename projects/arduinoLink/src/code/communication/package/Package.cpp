767b0a822110ab9a16ebf2665f784d05969e1f44
technik <cjtech90@gmail.com>
2012-11-23 18:01:22 +0100

    Arduino Link packages (To be tested)

---
+++ projects/arduinoLink/src/code/communication/package/Package.cpp
@@ -0,0 +1,47 @@
+//----------------------------------------------------------------------------------------------------------------------
+// Revolution SDK
+// Created by Carmelo J. Fdez-Agüera Tortosa a.k.a. (Technik)
+// On November 22nd, 2012
+//----------------------------------------------------------------------------------------------------------------------
+// Arduino link's communication package
+
+#include "package.h"
+
+//----------------------------------------------------------------------------------------------------------------------
+Package::Package(Type _type, uint8_t _length, const void* _data)
+	:mType(_type)
+{
+	setSize(_length);
+	for(unsigned i = 0; i < _length; ++i)
+		addData(reinterpret_cast<const uint8_t*>(_data)[i]);
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+Package::Package(const Package& _x)
+	:mType(_x.mType)
+{
+	setSize(_x.mDataSize);
+	for(unsigned i = 0; i < _x.mDataSize; ++i)
+		addData(_x.mData[i]);
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+Package::~Package()
+{
+	if(nullptr != mData)
+		delete[] mData;
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+void Package::setSize(uint8_t _size)
+{
+	mDataIdx = 0;
+	mDataSize = _size;
+	mData = new uint8_t[_size];
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+void Package::addData(uint8_t _data)
+{
+	mData[mDataIdx++] = _data;
+}
--- projects/arduinoLink/src/code/communication/package/package.h
+++ projects/arduinoLink/src/code/communication/package/package.h
@@ -5,6 +5,9 @@
 //----------------------------------------------------------------------------------------------------------------------
 // Arduino link's communication package
 
+#ifndef _ARDUINOLINK_COMMUNICATION_PACKAGE_PACKAGE_H_
+#define _ARDUINOLINK_COMMUNICATION_PACKAGE_PACKAGE_H_
+
 #include <cstdint>
 
 class Package
@@ -16,9 +19,65 @@ public:
 		data
 	};
 public:
-	Package(); // Empty package construction
-	Package(Type _type, uint8_t _length, const void* _data); // Build a package using data
-	Package(const Package&); // Copy constructor
-	Package(Package&&); // Move constructor
-	~Package();
+	Package		(); // Empty package construction
+	Package		(Type _type, uint8_t _length, const void* _data); // Build a package using data.
+	Package		(const Package&); // Copy constructor
+	Package		(Package&&); // Move constructor
+	~Package	();
+
+	// Data
+	void			setSize(uint8_t _size); // Sets size and allocates space for data
+	void			addData(uint8_t _byte);	// Assumes enough data is available in the packet
+	
+	// Accesors
+	Type			type	() const;
+	uint8_t			length	() const;
+	const uint8_t*	data	() const;
+
+private:
+	uint8_t*	mData;
+	uint8_t		mDataSize;
+	uint8_t		mDataIdx;
+	Type		mType;
 };
+
+//-------------------------------------------------------------------------------------------------------------------
+// Inline implementations
+
+//-------------------------------------------------------------------------------------------------------------------
+inline Package::Package()
+	:mData(nullptr)
+	,mDataSize(0)
+	,mDataIdx(0)
+{
+}
+
+//-------------------------------------------------------------------------------------------------------------------
+inline Package::Package(Package&& _x)
+	:mData(_x.mData)
+	,mDataSize(_x.mDataSize)
+	,mDataIdx(_x.mDataIdx)
+	,mType(_x.mType)
+{
+	_x.mData = nullptr;
+}
+
+//-------------------------------------------------------------------------------------------------------------------
+inline Package::Type Package::type() const
+{
+	return mType;
+}
+
+//-------------------------------------------------------------------------------------------------------------------
+inline uint8_t Package::length() const
+{
+	return mDataSize;
+}
+
+//-------------------------------------------------------------------------------------------------------------------
+inline const uint8_t* Package::data() const
+{
+	return mData;
+}
+
+#endif // _ARDUINOLINK_COMMUNICATION_PACKAGE_PACKAGE_H_
