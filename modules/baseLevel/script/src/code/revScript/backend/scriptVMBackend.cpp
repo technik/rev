053d51da3a262b82785cfdfa3674fa366fc62857
technik <cjtech90@gmail.com>
2013-01-21 17:35:17 +0100

    ScriptVM is now able to run code (extremely simple code).

---
+++ modules/baseLevel/script/src/code/revScript/backend/scriptVMBackend.cpp
@@ -0,0 +1,73 @@
+//-------------------------------------------------------------------------
+// revScript
+// Created by Carmelo J. Fdez-Agüera Tortosa (a.k.a. Technik)
+// On January 21st, 2013
+//-------------------------------------------------------------------------
+// Script Virtual Machine's backend is the interface between the bison generated parser
+// and the virtual machine itself.
+
+#include "scriptVMBackend.h"
+
+extern rev::script::ScriptVMBackend* gActiveBackend;
+
+int yyparse();
+
+namespace rev { namespace script {
+
+	//----------------------------------------------------------------------------------
+	ScriptVMBackend::ScriptVMBackend(ScriptVM* _owner)
+		:mOwner(_owner)
+		,mCodeBuffer(nullptr)
+		,mCodeCursor(0)
+	{ // Intentionally blank
+	}
+
+	//----------------------------------------------------------------------------------
+	void ScriptVMBackend::buildExecTree(const char* _code)
+	{
+		mCodeCursor = 0;
+		mCodeBuffer = _code;
+		gActiveBackend = this;
+		yyparse();
+	}
+
+	//----------------------------------------------------------------------------------
+	void ScriptVMBackend::matchStatement()
+	{
+		// TODO
+	}
+
+	//----------------------------------------------------------------------------------
+	void ScriptVMBackend::matchExpression()
+	{
+		// TODO
+	}
+
+	//----------------------------------------------------------------------------------
+	void ScriptVMBackend::matchSum(int, int)
+	{
+		// TODO
+	}
+
+	//----------------------------------------------------------------------------------
+	void ScriptVMBackend::matchInteger(int)
+	{
+		// TODO
+	}
+
+	//----------------------------------------------------------------------------------
+	unsigned ScriptVMBackend::retrieveCode(char* _dst, unsigned _maxSize)
+	{
+		for(unsigned i = 0; i < _maxSize; ++i)
+		{
+			if(mCodeBuffer[mCodeCursor+i] == '\0') {
+				return i;
+			} else {
+				_dst[i] = mCodeBuffer[mCodeCursor+i];
+			}
+		}
+		return _maxSize;
+	}
+
+}	// namespace script
+}	// namespace rev
