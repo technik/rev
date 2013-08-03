eb675af4a4ba64d1949d5b1245189c791903446c
technik <cjtech90@gmail.com>
2013-08-03 19:20:02 +0200

    Quick fixes in makefiles for AVR targets

--- common/make/avr/platform.mk
+++ common/make/avr/platform.mk
@@ -8,11 +8,15 @@
 PLATFORM := avr
 
 AVR_DEFINITIONS := -DATMEGA
-ifeq(atmega328p, $(MCU))
+ifeq (atmega328p, $(MCU))
     PLATFORM_DEFINITIONS := $(AVR_DEFINITIONS) -DATMEGA328P -DF_CPU=16000000L
     PLATFORM_FLAGS := -mmcu=m328p
+	PROG_PROTOCOL ?= arduino
+	PORT ?= COM3
 endif
-ifeq(atmega2560, $(MCU))
+ifeq (atmega2560, $(MCU))
     PLATFROM_DEFINITIONS := -DATMEGA -DATMEGA2560 -DF_CPU=16000000L
     PLATFORM_FLAGS := -mmcu=atmega2560
+	PROG_PROTOCOL ?= wiring
+	PORT ?= COM4
 endif
--- common/make/binary-common.mk
+++ common/make/binary-common.mk
@@ -9,7 +9,7 @@ include $(REVSDK_HOME)/common/make/$(PLATFORM)/toolchain.mk
 include $(REVSDK_HOME)/common/make/$(PLATFORM)/platform.mk
 
 CXX_SRC := $(shell find $(CODE_DIR) -name *.cpp)
-CXX_OBJ := $(patsubst %.cpp, %.cpp.o, $(CXX_SRC))
+CXX_OBJ := $(patsubst %.cpp, %.cpp.$(PLATFORM).o, $(CXX_SRC))
 
 TEMP_FILES := $(CXX_OBJ)
 
@@ -25,3 +25,49 @@ COMMON_C_FLAGS := $(PLATFORM_FLAGS) $(PREPROCESSOR_DEFINITIONS) $(WARNING_FLAGS)
  			 $(INCLUDE_FLAGS) $(DEBUG_FLAGS) $(OPTIMIZATION_FLAGS)
 CC_FLAGS := $(COMMON_C_FLAGS)
 CXX_FLAGS := $(COMMON_C_FLAGS) $(CXX_COMPILE_FLAGS)
+
+BUILD_DIR := build/$(MCU)/
+ASM_DIR := asm/$(MCU)/
+OUT_DIR := $(ROOT_DIR)/bin/$(MCU)
+
+OUT_NAME := $(OUT_DIR)/$(PRJ_NAME)
+OUTPUT := $(OUT_NAME).hex
+
+# -- Rules --
+all: $(OUTPUT)
+
+clean:
+	rm -f $(OUTPUT) $(TEMP_FILES) $(OUT_NAME).elf
+
+run: $(OUTPUT)
+	$(AVRDUDE) -p$(MCU) -c$(PROG_PROTOCOL) -b115200 -P$(PORT) -D -Uflash:w:$^:i
+
+%.hex: %.elf
+	$(OBJCOPY) -O ihex -R .eeprom -R .fuse -R .lock -R .signature $^ $@
+
+$(OUT_NAME).elf: $(CXX_OBJ)
+	$(CXX) -o $@ $^ $(CXX_FLAGS) $(REV_INCLUDE) $(LIB_DIR) -lc -lm -lc
+
+
+$(ASM_DIR)%.cpp.s: $(CODE_DIR)%.cpp
+	$(CXX) -S -o $@ $^ $(CXX_FLAGS) $(REV_INCLUDE) $(LIB_DIR)
+
+
+%.cpp.$(PLATFORM).o: %.cpp
+	$(CXX) -c -o $@ $^ $(CXX_FLAGS) $(REV_INCLUDE) $(LIB_DIR) -lc -lm -lc
+
+readelf: $(OUT_NAME).elf
+	$(READELF) -a $^
+
+dump: $(OUT_NAME).elf
+	$(OBJDUMP) -h $^
+
+dwarf: $(OUT_NAME).elf
+	$(OBJDUMP) --dwarf=info $^
+
+assembly: (OUT_NAME).S
+
+$(OUT_NAME).S: $(CXX_SRC)
+	$(CXX) -S -o $@ $^ $(CXX_FLAGS)
+
+.PHONY: readelf run clean
