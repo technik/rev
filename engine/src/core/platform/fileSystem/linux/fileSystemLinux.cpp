2b4b5284bb8caeb530e096b7bd3ca3e75f5b18cd
Technik <cjtech90@gmail.com>
2014-10-29 11:24:59 +0100

    Dummy implementation for linux file system

---
+++ engine/src/core/platform/fileSystem/linux/fileSystemLinux.cpp
@@ -0,0 +1,30 @@
+//----------------------------------------------------------------------------------------------------------------------
+// Revolution Engine
+// Created by Carmelo J. Fdez-Agüera Tortosa (a.k.a. Technik)
+// 2014/April/18
+//----------------------------------------------------------------------------------------------------------------------
+// File system interface for windows
+#ifdef __linux__
+
+#include "fileSystemLinux.h"
+#include <iostream>
+
+namespace rev {
+	namespace core {
+
+		//------------------------------------------------------------------------------------------------------------------
+		void FileSystemLinux::update()
+		{
+			// 666 TODO
+		}
+
+		//--------------------------------------------------------------------------------------------------------------
+		FileSystemLinux::FileSystemLinux()
+		{
+			// 666 TODO
+		}
+		
+	}	// namespace core
+}	// namespace rev
+
+#endif // __linux__
