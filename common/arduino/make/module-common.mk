8e830170f17578db0e0f5a4dc15f9969c597fdf8
technik <cjtech90@gmail.com>
2015-05-20 13:23:52 +0200

    Deleted deprecated build system files. Common files used in the deprecated make build system have been deleted to avoid confusions.

--- common/arduino/make/module-common.mk
+++
@@ -1,8 +0,0 @@
-#-----------------------------------------------------------------------------------------------------------------------
-# Revolution SDK
-# Created by Carmelo J. Fdez-Agüera Tortosa (a.k.a. Technik)
-# On October 8th, 2012
-#-----------------------------------------------------------------------------------------------------------------------
-# Arduino module common makefile
-
-include $(REVSDK_HOME)/common/make/module-common.mk
