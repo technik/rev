ae3d94e0bfb1357db8c41d22758b8453d73f3b93
technik <cjtech90@gmail.com>
2015-10-20 21:13:54 +0200

    Created render mesh.

---
+++ engine/src/video/graphics/renderer/renderMesh.h
@@ -0,0 +1,52 @@
+//----------------------------------------------------------------------------------------------------------------------
+// Revolution Engine
+// Created by Carmelo J. Fdez-Agüera Tortosa (a.k.a. Technik)
+//----------------------------------------------------------------------------------------------------------------------
+// Base renderable object
+#ifndef _REV_VIDEO_GRAPHICS_RENDERER_RENDEROBJ_H_
+#define _REV_VIDEO_GRAPHICS_RENDERER_RENDEROBJ_H_
+
+#include <cstdint>
+#include <math/algebra/matrix.h>
+#include <math/algebra/vector.h>
+
+namespace rev {
+	namespace video {
+
+		class RenderMesh {
+		public:
+			math::Vec3f*	vertices = nullptr;
+			math::Vec3f*	normals = nullptr;
+			math::Vec2f*	uvs = nullptr;
+			uint16_t		nVertices = 0;
+			uint16_t		nIndices = 0;
+			uint16_t*		triStrip = nullptr;
+			uint16_t		stripLength = 0;
+			uint16_t*		indices = nullptr;
+
+			//----------------------------------------------------------------------------------------------------------
+			void setVertexData(uint16_t _nVertices, math::Vec3f* _vertices
+				, math::Vec3f* _normals, math::Vec2f* _uvs) {
+				vertices = _vertices;
+				normals = _normals;
+				uvs = _uvs;
+				nVertices = _nVertices;
+			}
+
+			//----------------------------------------------------------------------------------------------------------
+			void setFaceIndices(uint16_t _nIndices, uint16_t* _indices, bool _strip = false) {
+				if (_strip) {
+					stripLength = _nIndices;
+					triStrip = _indices;
+				}
+				else {
+					nIndices = _nIndices;
+					indices = _indices;
+				}
+			}
+
+		};
+	}	// namespace video
+}	// namespace rev
+
+#endif // _REV_VIDEO_GRAPHICS_RENDERER_RENDEROBJ_H_
