414087129514629f78c0be3ec216f6afedad63d1
technik <cjtech90@gmail.com>
2016-08-16 11:20:24 +0200

    Frustum interface.

--- engine/src/math/geometry/types.h
+++ engine/src/math/geometry/types.h
@@ -5,6 +5,7 @@
 #ifndef _REV_MATH_GEOMETRY_TYPES_H_
 #define _REV_MATH_GEOMETRY_TYPES_H_
 
+#include <math/algebra/matrix.h>
 #include <math/algebra/vector.h>
 
 namespace rev {
@@ -18,6 +19,21 @@ namespace rev {
 			BBox(const Vec3f& _min, const Vec3f& _max) : min(_min), max(_max) {}
 
 			Vec3f size() const { return max - min; }
+		};
+
+		struct Frustum {
+			Frustum(float _aspectRatio, float _fov, float _near, float _far);
+
+			const Mat44f& projection() const;
+			const Vec3f*	vertices() const;
+
+		private:
+			float _aspectRatio;
+			float _fov;
+			float _near;
+			float _far;
+			Vec3f vertices[8];
+			Mat44f projection;
 		};
 
 } }	// namespace rev::math
