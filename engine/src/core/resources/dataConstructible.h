239f0349deb0b1832f776f4e640386f630c2f448
technik <cjtech90@gmail.com>
2017-05-23 20:22:08 +0200

    Implemented factory registration

--- engine/src/core/resources/dataConstructible.h
+++ engine/src/core/resources/dataConstructible.h
@@ -13,10 +13,10 @@ namespace rev {
 
 		template<class Derived_>
 		struct DataConstructible {
-			static Derived_* construct(const cjson::Json&);
-			
 			typedef std::function<TransformSrc*(const cjson::Json&)>	Factory;
-			static void registerFactory(Factory);
+			
+			static Derived_* construct(const cjson::Json&);
+			static void registerFactory(const std::string&, Factory);
 
 		private:
 			static std::map<std::string, Factory>	sFactories;
@@ -31,5 +31,10 @@ namespace rev {
 				return (iter->second)(_data);
 			}
 			return nullptr; // Unable to locate factory
+		}
+
+		template<class Derived_>
+		void DataConstructible<Derived_>::registerFactory(const std::string& _key, Factory _factory) {
+			sFactories.insert(std::make_pair(_key, _factory));
 		}
 } }
