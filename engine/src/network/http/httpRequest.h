47375277e758c939f4a927721f361891b3761b10
technik <cjtech90@gmail.com>
2017-05-30 12:39:42 +0200

    http request interface

---
+++ engine/src/network/http/httpRequest.h
@@ -0,0 +1,44 @@
+//----------------------------------------------------------------------------------------------------------------------
+// Revolution Engine
+// Created by Carmelo J. Fdez-Agüera Tortosa (a.k.a. Technik)
+//----------------------------------------------------------------------------------------------------------------------
+#pragma once
+
+#include <string>
+#include <unordered_map>
+#include <vector>
+#include "httpMessage.h"
+#include <cjson/json.h>
+
+namespace rev {
+	namespace net {
+		namespace http {
+
+			class Request : public Message {
+			public:
+				enum METHOD {
+					Get,
+					Post,
+					Put
+				};
+				// Construction
+				Request(METHOD, const std::string& _url, // Status line
+					const std::string& _body); // Body
+				Request(const std::string& _rawRequest);
+				virtual ~Request() = default;
+
+				// Accessors
+				METHOD				method() const { return mMethod; }
+				const std::string&	url() const { return mUrl; }
+
+				static Request		jsonRequest(METHOD, const std::string& _url, const cjson::Json& _payload);
+
+			private:
+				int		processMessageLine(const std::string& _raw) override;
+				void	serializeMessageLine(std::string& _dst) const override;
+
+			private:
+				METHOD mMethod;
+				std::string mUrl;
+			};
+}}}
