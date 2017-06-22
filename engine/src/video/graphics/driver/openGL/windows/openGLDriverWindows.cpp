a3a34ee69420871343c1cc141a2ac6665cd141a9
technik <cjtech90@gmail.com>
2017-06-22 21:29:40 +0200

    Fixed pixel format description for opengl context creation

--- engine/src/video/graphics/driver/openGL/windows/openGLDriverWindows.cpp
+++ engine/src/video/graphics/driver/openGL/windows/openGLDriverWindows.cpp
@@ -14,6 +14,7 @@
 #include <string>
 #include <gl/GL.h>
 #include <core/platform/fileSystem/fileSystem.h>
+#include <iostream>
 
 using namespace std;
 using namespace rev::core;
@@ -24,7 +25,9 @@ namespace rev {
 		OpenGLDriverWindows::OpenGLDriverWindows(Window* _window)
 		{
 			mWindowHandle = _window->winapiHandle();
+			mDevCtxHandle = GetDC(mWindowHandle); // Device contex
 
+			// Set pixel format
 			static PIXELFORMATDESCRIPTOR pfd =				// pfd Tells Windows How We Want Things To Be
 			{
 				sizeof(PIXELFORMATDESCRIPTOR),				// Size Of This Pixel Format Descriptor
@@ -33,24 +36,29 @@ namespace rev {
 				PFD_SUPPORT_OPENGL |						// Format Must Support OpenGL
 				PFD_DOUBLEBUFFER,							// Must Support Double Buffering
 				PFD_TYPE_RGBA,								// Request An RGBA Format
-				16,											// Color Depth
+				32,											// Color Depth
 				0, 0, 0, 0, 0, 0,							// Color Bits Ignored
 				0,											// No Alpha Buffer
 				0,											// Shift Bit Ignored
 				0,											// No Accumulation Buffer
 				0, 0, 0, 0,									// Accumulation Bits Ignored
-				24,											// 16Bit Z-Buffer (Depth Buffer)  
-				0,											// No Stencil Buffer
+				24,											// 24 Bit Z-Buffer (Depth Buffer)  
+				8,											// No Stencil Buffer
 				0,											// No Auxiliary Buffer
 				PFD_MAIN_PLANE,								// Main Drawing Layer
 				0,											// Reserved
 				0, 0, 0										// Layer Masks Ignored
 			};
 
-			// -- Some Windows-OpenGL configuration --
-			mDevCtxHandle = GetDC(mWindowHandle); // Device context
 			GLuint pixelFormat = ChoosePixelFormat(mDevCtxHandle, &pfd);
+			DescribePixelFormat(mDevCtxHandle, pixelFormat, sizeof(PIXELFORMATDESCRIPTOR), &pfd);
+			std::cout << "Retrieved pixel format:\n"
+				<< "Color depth: " << (size_t)pfd.cColorBits << "\n"
+				<< "Z-Buffer depth: " << (size_t)pfd.cDepthBits << "\n"
+				<< "Stencil depth: " << (size_t)pfd.cStencilBits << "\n";
 			SetPixelFormat(mDevCtxHandle, pixelFormat, &pfd);
+
+			// Set context
 			HGLRC renderCtxHandle = wglCreateContext(mDevCtxHandle);
 			wglMakeCurrent(mDevCtxHandle, renderCtxHandle);
 
