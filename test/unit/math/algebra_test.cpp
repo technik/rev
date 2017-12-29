5babcf1c6e0d3af6a8399ba0b491decafce81dc1
technik <cjtech90@gmail.com>
2017-12-29 21:16:11 +0100

    Prepared a test for basic matrix operations

--- test/unit/math/algebra_test.cpp
+++ test/unit/math/algebra_test.cpp
@@ -9,8 +9,25 @@ using namespace rev::math;
 //----------------------------------------------------------------------------------------------------------------------
 // Test operators on Vec3
 void testVec3() {
-	Vec3f a = Vec3f(1.f);
-	Vec3f b = Vec3f(2.f);
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+// Test basic matrix operations
+void testMatrix() {
+	auto ones = Mat44f::ones();
+	auto iden = Mat44f::identity();
+	auto zero = Mat44f::zero();
+	auto two = ones + ones;
+	auto a = two * iden;
+	assert(a == two);
+	a = two - zero();
+	assert(a == two);
+	a = ones * 2;
+	assert(two == a);
+	auto four = two.cwiseProduct(two);
+	assert(four == (4.f * ones));
+	a = iden.cwiseMax(-iden);
+	assert(a == iden);
 }
 
 int main() {
