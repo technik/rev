b2852b286768ddf89d8e2b3f19017c14d6340d18
technik <cjtech90@gmail.com>
2018-01-01 17:39:01 +0100

    WIP: Create shaders

--- engine/src/graphics/driver/shader.cpp
+++ engine/src/graphics/driver/shader.cpp
@@ -2,11 +2,19 @@
 // Revolution Engine
 //----------------------------------------------------------------------------------------------------------------------
 #include "shader.h"
+#include "openGL/openGL.h"
 
 namespace rev { namespace graphics {
 
 	std::unique_ptr<Shader>	Shader::createShader(const char* code) {
+		GLuint vertexShader = 0, fragmentShader = 0;
+		if(!createSubprogram(code, GL_VERTEX_SHADER, vertexShader) ||
+			!createSubprogram(code, GL_VERTEX_SHADER, vertexShader))
+		{
+			return nullptr;
+		}
 
+		auto program = glCreateProgram();
 
 		return nullptr;
 	}
--- engine/src/graphics/driver/shader.h
+++ engine/src/graphics/driver/shader.h
@@ -3,12 +3,18 @@
 //----------------------------------------------------------------------------------------------------------------------
 #pragma once
 #include <memory>
+#include "openGL/openGL.h"
 
 namespace rev { namespace graphics {
 
 	class Shader {
 	public:
 		static std::unique_ptr<Shader>	createShader(const char* code);
+
+	private:
+		static bool createSubprogram(const char* _code, GLenum _shaderType, GLuint& _dst);
+
+		GLuint mGLProgram = 0;
 	};
 
 }}	// namespace rev::graphics
