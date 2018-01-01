c369edb9d344569d19a8951ee5daef21fd715c4c
technik <cjtech90@gmail.com>
2018-01-01 13:08:23 +0100

    Init graphics driver

--- player/player.cpp
+++ player/player.cpp
@@ -1,8 +1,26 @@
 //----------------------------------------------------------------------------------------------------------------------
 // Revolution Engine
 //----------------------------------------------------------------------------------------------------------------------
+#include <cassert>
 #include "player.h"
 
 namespace rev {
+
+	//------------------------------------------------------------------------------------------------------------------
+	bool Player::init(Window _window) {
+		assert(!mGfxDriver);
+
+		mGfxDriver = graphics::GraphicsDriverGL::createDriver(_window);
+		return mGfxDriver != nullptr;
+	}
+
+	//------------------------------------------------------------------------------------------------------------------
+	bool Player::update() {
+		if(!mGfxDriver)
+			return true;
+
+		mGfxDriver->swapBuffers();
+		return true;
+	}
 
 }	// namespace rev
--- player/player.h
+++ player/player.h
@@ -10,10 +10,13 @@ namespace rev {
 	class Player {
 	public:
 		typedef rev::graphics::GraphicsDriverGL::NativeWindow	Window;
-		bool init(Window) { return true; }
+		bool init(Window);
 
 		// Common components
-		bool update() { return true; }
+		bool update();
+
+	private:
+		graphics::GraphicsDriverGL* mGfxDriver = nullptr;
 	};
 
 }	// namespace rev
