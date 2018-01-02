cdc2f5f05921666af69f370e9afd9cd3bfdd4560
technik <cjtech90@gmail.com>
2018-01-02 21:08:44 +0100

    Fixed sample shader

--- player/player.cpp
+++ player/player.cpp
@@ -17,27 +17,13 @@ namespace rev {
 				R"(
 #ifdef VTX_SHADER
 			layout(location = 0) in vec2 vertex;
-			//layout(location = 1) in vec3 normal;
-			//layout(location = 2) in float material;
-
-			//layout(location = 0) uniform mat4 uWVP;
-
-			//layout(location = 0) out float light;
-			//layout(location = 1) out float vMat;
 			
 			void main ( void )
 			{
-				gl_Position = uWVP * vec4(vertex, 1.0);
-				vMat = material;
-				light = min(max(dot(normal, normalize(vec3(0.1, -0.3, 0.8))), 0.0)+0.3, 1.0);
+				gl_Position = vec4(vertex, 0.0, 1.0);
 			}
 #endif
 #ifdef PXL_SHADER
-			//layout(location = 0) in float light;
-			//layout(location = 1) in float vMat;
-
-			//vec3 material[4] = { vec3(0.7,0.5,0.2), vec3(0.0,0.5,0.0), vec3(0.0,0.5,0.0), vec3(0.4, 0.4, 0.4) };
-
 			out vec3 color;
 			void main (void) {
 				
