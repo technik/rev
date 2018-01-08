77d5651a2c4145f69d93c779bab566d6ba23748b
technik <cjtech90@gmail.com>
2018-01-08 23:00:10 +0100

    Model processor: Multi object fbx -> scn

--- tools/modelProcessor/modelProcessor.cpp
+++ tools/modelProcessor/modelProcessor.cpp
@@ -6,20 +6,23 @@
 #include <assimp/scene.h>           // Output data structure
 #include <assimp/postprocess.h>     // Post processing flags
 
+#include <math/algebra/vector.h>
+
 #include <cassert>
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
-/*
+using namespace rev::math;
 
 // --- Global data ---
 // The importer is stored globally, so buffers don't get erased and we can save some memcpy's
 Assimp::Importer fbxLoader;
 
 struct IntermediateModel {
-	VertexFormat format;
+	//VertexFormat format;
 	Vec3f* vertices = nullptr;
 	Vec3f* normals = nullptr;
 	Vec2f* uvs = nullptr;
@@ -28,34 +31,61 @@ struct IntermediateModel {
 	uint32_t nIndices = 0;
 	uint16_t nVertices = 0;
 
-	void collapseVertexData(void* _dst, size_t stride) {
-		size_t nOffset = sizeof(Vec3f);
-		size_t uvOffset = nOffset + sizeof(Vec3f);
+	struct VertexData {
+		Vec3f position, normal;
+		Vec2f uv;
+	};
+
+	void collapseVertexData(vector<VertexData>& _dst) {
 		for (size_t i = 0; i < nVertices; ++i) {
-			auto dataRow = &((uint8_t*)_dst)[stride*i];
-			auto v0 = (Vec3f*)dataRow;
-			auto n0 = (Vec3f*)(&dataRow[nOffset]);
-			auto uv0 = (Vec2f*)(&dataRow[uvOffset]);
-			*v0 = vertices[i];
-			*n0 = normals[i];
-			*uv0 = uvs[i];
+			auto& dataRow = _dst[i];
+			dataRow.position = vertices[i];
+			dataRow.normal = normals[i];
+			dataRow.uv = uvs[i];
 		}
+	}
+
+	//----------------------------------------------------------------------------------------------------------------------
+	bool loadFBXMesh(const aiMesh* _mesh) {
+		// Vertex data
+		nVertices = _mesh->mNumVertices;
+		bool hasNormals = _mesh->HasNormals();
+		bool hasUVs = _mesh->GetNumUVChannels() > 0;
+		if (!hasNormals || !hasUVs) {
+			cout << "Error: Mesh must have normals and uvs\n";
+			return false;
+		}
+		if (_mesh->GetNumUVChannels() > 1) {
+			cout << "Error: Too many uv channels. Only 1 is supported\n";
+			return false;
+		}
+		vertices = reinterpret_cast<Vec3f*>(_mesh->mVertices);
+		normals = reinterpret_cast<Vec3f*>(_mesh->mNormals);
+		uvs = new Vec2f[nVertices];
+		auto uv0 = _mesh->mTextureCoords[0];
+		for(auto i = 0; i < nVertices; ++i) {
+			uvs[i] = Vec2f(uv0[i].x, uv0[i].y);
+		}
+		// Index data
+		nIndices = _mesh->mNumFaces * 3;
+		indices = new uint16_t[nIndices];
+		for (size_t i = 0; i < _mesh->mNumFaces; ++i) {
+			indices[3 * i + 0] = _mesh->mFaces[i].mIndices[0];
+			indices[3 * i + 1] = _mesh->mFaces[i].mIndices[1];
+			indices[3 * i + 2] = _mesh->mFaces[i].mIndices[2];
+		}
+		return true;
 	}
 	
 	bool saveToStream(ostream& _out) {
-		auto stride = format.stride();
 		// Allocate space for cache-friendly vertex data
-		float* vertexData = reinterpret_cast<float*>(new uint8_t[stride*nVertices]);
-		if(!vertexData) {
-			cout << "Error: Unable to allocate memory\n";
-			return false;
-		}
-		collapseVertexData(vertexData, stride);
+		vector<VertexData> vertexData(nVertices);
+		collapseVertexData(vertexData);
 		// Save header
 		_out.write((char*)&nVertices, sizeof(nVertices));
 		_out.write((char*)&nIndices, sizeof(nIndices));
 		// Save vertex data
-		_out.write((const char*)vertexData, stride*(size_t)nVertices);
+		_out.write((const char*)vertexData.data(), sizeof(VertexData)*(size_t)nVertices);
 		if(!_out)
 			return false;
 		// Save index data
@@ -66,53 +96,59 @@ struct IntermediateModel {
 	}
 };
 
-//----------------------------------------------------------------------------------------------------------------------
-bool loadFBXMesh(const aiMesh* _mesh, IntermediateModel& _dst) {
-	// Vertex data
-	_dst.nVertices = _mesh->mNumVertices;
-	bool hasNormals = _mesh->HasNormals();
-	bool hasUVs = _mesh->GetNumUVChannels() > 0;
-	if (!hasNormals || !hasUVs) {
-		cout << "Error: Mesh must have normals and uvs\n";
-		return false;
+struct RenderObj {
+	int meshIdx = -1;
+	Mat44f transform;
+};
+
+struct Scene {
+	vector<IntermediateModel>	meshes;
+	vector<RenderObj>			objects;
+
+	bool saveToStream(ostream& _out) {
+		unsigned nMeshes = meshes.size();
+		unsigned nObjects = meshes.size();
+		// Save header
+		_out.write((char*)&nMeshes, sizeof(nMeshes));
+		_out.write((char*)&nObjects, sizeof(nObjects));
+		// Save meshes
+		for(auto& mesh : meshes)
+			mesh.saveToStream(_out);
+		// Save objects
+		_out.write((char*)objects.data(), sizeof(RenderObj)*objects.size());
+
+		return true;
 	}
-	if (_mesh->GetNumUVChannels() > 1) {
-		cout << "Error: Too many uv channels. Only 1 is supported\n";
-		return false;
-	}
-	_dst.vertices = reinterpret_cast<Vec3f*>(_mesh->mVertices);
-	_dst.normals = reinterpret_cast<Vec3f*>(_mesh->mNormals);
-	_dst.uvs = new Vec2f[_dst.nVertices];
-	auto uv0 = _mesh->mTextureCoords[0];
-	for(auto i = 0; i < _dst.nVertices; ++i) {
-		_dst.uvs[i] = Vec2f(uv0[i].x, uv0[i].y);
-	}
-	// Index data
-	_dst.nIndices = _mesh->mNumFaces * 3;
-	_dst.indices = new uint16_t[_dst.nIndices];
-	for (size_t i = 0; i < _mesh->mNumFaces; ++i) {
-		_dst.indices[3 * i + 0] = _mesh->mFaces[i].mIndices[0];
-		_dst.indices[3 * i + 1] = _mesh->mFaces[i].mIndices[1];
-		_dst.indices[3 * i + 2] = _mesh->mFaces[i].mIndices[2];
-	}
-	// Format
-	_dst.format.normals = VertexFormat::NormalFormat::normal3;
-	_dst.format.uvChannels = 1;
-	return true;
-}
+};
 
 //----------------------------------------------------------------------------------------------------------------------
-bool loadFBX(const string& _src, IntermediateModel& _dst) {
+bool loadFBX(const string& _src, Scene& _dst) {
 	const aiScene* fbx = fbxLoader.ReadFile(_src, aiProcess_JoinIdenticalVertices | aiProcess_PreTransformVertices);
 	if(!fbx)
 		return false;
-	const aiNode* sceneRoot = fbx->mRootNode;
-	if(sceneRoot->mNumMeshes == 0)
+	if(fbx->HasMeshes())
 	{
-		cout << "Warning: node has no meshes. Nothing will be exported\n";
-		return true;
+		// Load all meshes
+		_dst.meshes.resize(fbx->mNumMeshes);
+		for(size_t i = 0; i < _dst.meshes.size(); ++i)
+		{
+			auto& fbxMesh = fbx->mMeshes[i];
+			_dst.meshes[i].loadFBXMesh(fbxMesh);
+		}
+		// Load scene nodes
+		int parent = -1;
+		const aiNode* sceneRoot = fbx->mRootNode;
+		_dst.objects.resize(sceneRoot->mNumChildren);
+		for(size_t i = 0; i < sceneRoot->mNumChildren; ++i)
+		{
+			auto& obj = _dst.objects[i];
+			const auto& srcObj = sceneRoot->mChildren[i];
+			obj.transform.setIdentity();
+			if(srcObj->mNumMeshes > 0)
+				obj.meshIdx = srcObj->mMeshes[0];
+		}
 	}
-	return loadFBXMesh(fbx->mMeshes[sceneRoot->mMeshes[0]], _dst);
+	return true;
 }
 
 //----------------------------------------------------------------------------------------------------------------------
@@ -125,18 +161,18 @@ int main(int _argc, const char** _argv) {
 	}
 	string modelName = _argv[1];
 	string src = modelName + ".fbx";
-	string dst = modelName + ".rmd";
+	string dst = modelName + ".scn";
 	cout << src << " >> " << dst << "\n";
 
-	IntermediateModel uncompressedModel;
-	if (!loadFBX(src, uncompressedModel))
+	Scene scene;
+	if (!loadFBX(src, scene))
 	{
 		cout << "Error loading model: " << src << "\n";
 		return -1;
 	}
 	ofstream outFile(dst, std::ofstream::binary);
 	if (outFile.is_open()) {
-		uncompressedModel.saveToStream(outFile);
+		scene.saveToStream(outFile);
 		outFile.close();
 	}
 	else {
@@ -145,5 +181,3 @@ int main(int _argc, const char** _argv) {
 	}
 	return 0;
 }
-
-*/
