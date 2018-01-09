ed3a64c6264bca2f0d5537d96449cc45cb01687b
technik <cjtech90@gmail.com>
2018-01-09 21:35:13 +0100

    Load shader from file

--- player/player.cpp
+++ player/player.cpp
@@ -36,31 +36,8 @@ namespace rev {
 		if(mGfxDriver) {
 			glClearColor(89.f/255.f,235.f/255.f,1.f,1.f);
 			// Create shader
-			mShader = Shader::createShader(
-				R"(
-#ifdef VTX_SHADER
-			layout(location = 0) in vec3 vertex;
-			layout(location = 1) in vec3 normal;
-			layout(location = 0) uniform mat4 uWorldViewProjection;
-
-			out vec3 vtxColor;			
-
-			void main ( void )
-			{
-				vtxColor = normal;
-				gl_Position = uWorldViewProjection * vec4(vertex, 1.0);
-			}
-#endif
-#ifdef PXL_SHADER
-			out lowp vec3 color;
-			in vec3 vtxColor;			
-			void main (void) {
-				float light = max(0.0,dot(normalize(vec3(0.2, -0.3, 1.0)),normalize(vtxColor)));
-				color = light * vec3(255.0/255.0,22.0/255.0,88.0/255.0) + 0.2*vec3(89.0/255.0,235.0/255.0,1.0);
-			}
-#endif
-				)"
-			);
+			core::File shaderFile("pbr.fx");
+			mShader = Shader::createShader(shaderFile.bufferAsText());
 			if(mShader)
 				mShader->bind();
 			// Camera
@@ -148,10 +125,6 @@ namespace rev {
 		view.block<3,4>(0,0) = camera.matrix();
 		auto vp = mProjectionMtx * view;
 		auto worldMatrix = Mat44f::identity();
-
-		// For each render obj
-		/*mTriangle->component<Transform>()->xForm.setRotation(math::Quatf(Vec3f(0.f,0.f,1.f), t));
-		mTriangle->update(dt);*/
 
 		for(auto renderable : mGraphicsScene.renderables()) {
 			auto& renderObj = renderable->renderObj();
