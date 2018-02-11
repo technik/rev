daa889ad68a59024df56aa093966e071f4eb6c7d
technik <cjtech90@gmail.com>
2018-02-11 22:15:00 +0100

    Reduce renderScene's interface. Mesh and material pools are no longer a part of it.

--- engine/src/graphics/scene/renderScene.h
+++ engine/src/graphics/scene/renderScene.h
@@ -20,33 +20,12 @@ namespace rev { namespace graphics {
 			mLightClr = math::Vec3f::ones();
 		}
 
-		// Content handling
-		size_t registerMaterial(std::shared_ptr<Material> material)
-		{
-			mMaterials.push_back(material);
-			return mMaterials.size() - 1;
-		}
-
-		size_t registerMesh(std::shared_ptr<RenderGeom> mesh)
-		{
-			mMeshes.push_back(mesh);
-			return mMeshes.size() - 1;
-		}
-
-		std::shared_ptr<RenderObj> createRenderObj(uint32_t meshId, uint32_t materialId);
-		std::shared_ptr<RenderObj> createRenderObj(const std::vector<std::pair<uint32_t,uint32_t>>& meshes);
-
-		const std::vector<std::shared_ptr<Material>>	materials() const { return mMaterials; }
-
 		// Accessors
 		const math::Vec3f& lightClr() const { return mLightClr; }
 		auto renderables() const { return mRenderables; }
 
 	private:
-		math::Vec3f					mLightClr;
-
-		std::vector<std::shared_ptr<RenderGeom>>	mMeshes;
-		std::vector<std::shared_ptr<Material>>		mMaterials;
+		math::Vec3f									mLightClr;
 		std::vector<std::weak_ptr<RenderObj>>		mRenderables;
 	};
 
