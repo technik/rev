315fb5228ef5d2ecfa1cc1a37c5c634e7fb415ec
technik <cjtech90@gmail.com>
2018-08-12 20:02:57 +0200

    Basic environment probe class.

---
+++ engine/src/graphics/scene/EnvironmentProbe.cpp
@@ -0,0 +1,51 @@
+//--------------------------------------------------------------------------------------------------
+// Revolution Engine
+//--------------------------------------------------------------------------------------------------
+// Copyright 2018 Carmelo J Fdez-Aguera
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+// and associated documentation files (the "Software"), to deal in the Software without restriction,
+// including without limitation the rights to use, copy, modify, merge, publish, distribute,
+// sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all copies or
+// substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+// NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#include "EnvironmentProbe.h"
+#include <nlohmann/json.hpp>
+#include <fstream>
+#include <graphics/Image.h>
+#include <memory>
+#include <vector>
+
+using Json = nlohmann::json;
+
+namespace rev::graphics
+{
+	//------------------------------------------------------------------------------------------------------------------
+	EnvironmentProbe::EnvironmentProbe(const std::string& jsonName)
+	{
+		// Open json descriptor
+		Json probeDesc;
+		std::ifstream(jsonName) >> probeDesc;
+
+		// Load all images
+		m_numLevels = probeDesc.size();
+		std::vector<std::shared_ptr<const Image>> mipImages;
+		for(size_t i = 0; i < m_numLevels; ++i)
+		{
+			auto& desc = probeDesc[i];
+			auto image = Image::load(desc["name"], 3);
+			mipImages.push_back(image);
+		}
+
+		// Create a new texture with all the levels
+		m_texture = std::make_shared<Texture>(mipImages, true);
+	}
+}
