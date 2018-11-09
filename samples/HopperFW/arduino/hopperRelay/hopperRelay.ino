788fc2968d414743df28448bf91b9a83741063a5
technik <cjtech90@gmail.com>
2018-11-09 19:58:46 +0100

    Read data from imu

--- samples/HopperFW/arduino/hopperRelay/hopperRelay.ino
+++ samples/HopperFW/arduino/hopperRelay/hopperRelay.ino
@@ -1,5 +1,6 @@
 #include <arduino_etl.h>
 #include <Servo.h>
+#include <Wire.h>
 
 OutputPin<Pin13> led;
 Servo esc;
@@ -43,6 +44,40 @@ struct MsgProcessor
   }
 };
 
+const int MPU_addr=0x68;  // I2C address of the MPU-6050
+
+void setupSensor()
+{
+  Wire.begin();
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x6b);
+  Wire.write(0);
+  Wire.endTransmission(true);
+}
+
+void readSensor()
+{
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x3b);
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_addr,14,true);
+  auto AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)    
+  auto AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+  auto AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+  auto Tmp=Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+  auto GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+  auto GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+  auto GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+  Serial.print("AcX = "); Serial.println(AcX);
+  Serial.print("AcY = "); Serial.println(AcY);
+  Serial.print("AcZ = "); Serial.println(AcZ);
+  Serial.print("Tmp = "); Serial.println(Tmp/340.00+36.53);  //equation for temperature in degrees C from datasheet
+  Serial.print("GyX = "); Serial.println(GyX);
+  Serial.print("GyY = "); Serial.println(GyY);
+  Serial.print("GyZ = "); Serial.println(GyZ);
+  Serial.println("--------------");
+}
+
 MsgProcessor stateMachine;
 
 void setup() {
@@ -50,7 +85,10 @@ void setup() {
   Serial.begin(9600);
   esc.attach(10);
   esc.write(0);
+  setupSensor();
 }
+
+unsigned long lastMillis = 0;
 
 void loop() {
   // put your main code here, to run repeatedly:
@@ -63,5 +101,12 @@ void loop() {
       led.setLow();
 
     esc.write(map(stateMachine.pwm, 0, 255, 0, 180));
+  }
+  // Timed tasks
+  auto t = millis();
+  if(t - lastMillis > 1000)
+  {
+    lastMillis = t;
+    readSensor();
   }
 }
