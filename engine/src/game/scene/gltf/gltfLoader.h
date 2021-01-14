b1be534762fbf82cf9c3a47ada336717641719b2
Carmelo Fernandez Aguera <carferna@microsoft.com>
2021-01-13 19:30:05 -0800

    Clean gltf loader interface.

--- engine/src/game/scene/gltf/gltfLoader.h
+++ engine/src/game/scene/gltf/gltfLoader.h
@@ -19,25 +19,18 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #pragma once
 
-#include <string>
 #include "../sceneNode.h"
-#include <graphics/backend/texture2d.h>
-#include <graphics/scene/renderScene.h>
-#include <graphics/scene/renderGeom.h>
 #include <memory>
+#include <string>
 #include <vector>
 
 namespace fx::gltf { struct Document; }
-
 namespace rev::game { class SceneNode; }
 
-namespace rev::gfx {
-	class Animation;
-	class Device;
-	class Effect;
-	class Material;
-	class RenderMesh;
-	class SkinInstance;
+namespace rev::gfx
+{
+	class RenderContextVulkan;
+	class VulkanAllocator;
 }
 
 namespace rev::game {
@@ -45,69 +38,19 @@ namespace rev::game {
 	class GltfLoader
 	{
 	public:
-		GltfLoader(gfx::Device& gfx)
-			:m_gfxDevice(gfx)
-		{}
-
+		GltfLoader(gfx::RenderContextVulkan&);
 		~GltfLoader();
 
 		/// Load a gltf scene
-		/// Add renderable content to _gfxWorld
-		/// filePath must not contain folder, file name and extension
-		/// If parentNode is not nullptr, all the scene nodes will be added as children to it
-		void load(
-			SceneNode& parentNode,
-			const std::string& filePath,
-			gfx::RenderScene& _gfxWorld,
-			std::vector<std::shared_ptr<SceneNode>>& animNodes,
-			std::vector<std::shared_ptr<gfx::Animation>>& _animations);
+		/// filePath must contain folder, file name and extension
+		/// \return root node of the loaded asset
+		std::shared_ptr<SceneNode> load(const std::string& filePath);
 
 	private:
-		// Shared resources
-		std::shared_ptr<gfx::Effect> metallicRoughnessEffect();
-		std::shared_ptr<gfx::Effect> specularEffect();
-		std::shared_ptr<gfx::Material> defaultMaterial();
+		gfx::RenderContextVulkan& m_renderContext;
+		gfx::VulkanAllocator& m_alloc;
 
-		gfx::Texture2d getTexture(
-			const fx::gltf::Document& document,
-			gfx::TextureSampler defaultSampler,
-			int32_t index,
-			bool sRGB,
-			int nChannels = 0);
-
-		// Load resources
-		std::vector<std::shared_ptr<gfx::Material>> loadMaterials(
-			const fx::gltf::Document& _document
-		);
-
-		std::vector<std::shared_ptr<gfx::RenderMesh>> loadMeshes(
-			const std::vector<gfx::RenderGeom::Attribute>& attributes,
-			const fx::gltf::Document& _document,
-			const std::vector<std::shared_ptr<gfx::Material>>& _materials);
-
-		std::vector<std::shared_ptr<game::SceneNode>> loadNodes(
-			const fx::gltf::Document& _document,
-			const std::vector<std::shared_ptr<gfx::RenderMesh>>& _meshes,
-			const std::vector<std::shared_ptr<gfx::SkinInstance>>& _skins,
-			const std::vector<std::shared_ptr<gfx::Material>>& _materials,
-			gfx::RenderScene& _gfxWorld);
-
-		void loadImages(const fx::gltf::Document&);
-
-	private:
-		gfx::Device& m_gfxDevice;
 		std::string m_assetsFolder;
-
-		// Cached resources
-		gfx::Texture2d m_invalidTexture;
-
-		std::vector<gfx::Texture2d> m_textures;
-		std::vector<std::shared_ptr<gfx::Image>> m_loadedImages;
-
-		// Supported effects
-		std::shared_ptr<gfx::Effect> m_metallicRoughnessEffect;
-		std::shared_ptr<gfx::Effect> m_specularEffect;
-		std::shared_ptr<gfx::Material> m_defaultMaterial;
 	};
 
 }
