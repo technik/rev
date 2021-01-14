e83e1b6ab479e74095b87d272e767f0c9df1341d
Carmelo Fernandez Aguera <carferna@microsoft.com>
2021-01-13 18:54:42 -0800

    Fixed device memory type selection.

--- samples/gltfViewer/src/player.cpp
+++ samples/gltfViewer/src/player.cpp
@@ -154,8 +154,8 @@ namespace rev {
 
 		// Allocate buffers
 		auto& alloc = renderContext().allocator();
-		m_vtxPosBuffer = alloc.createGpuBuffer(sizeof(Vec2f) * numVertices, vk::BufferUsageFlagBits::eTransferDst | vk::BufferUsageFlagBits::eVertexBuffer);
-		m_vtxClrBuffer = alloc.createGpuBuffer(sizeof(Vec3f) * numVertices, vk::BufferUsageFlagBits::eTransferDst | vk::BufferUsageFlagBits::eVertexBuffer);
+		m_vtxPosBuffer = alloc.createGpuBuffer(sizeof(Vec2f) * numVertices, vk::BufferUsageFlagBits::eTransferDst | vk::BufferUsageFlagBits::eVertexBuffer, renderContext().graphicsQueueFamily());
+		m_vtxClrBuffer = alloc.createGpuBuffer(sizeof(Vec3f) * numVertices, vk::BufferUsageFlagBits::eTransferDst | vk::BufferUsageFlagBits::eVertexBuffer, renderContext().graphicsQueueFamily());
 
 		// Copy data to the GPU
 		//alloc.copyToGPU(*m_vtxPosBuffer, vtxPos, numVertices);
