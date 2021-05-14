b7578d13d81357887a807caf467d5f2b8b042e35
Carmelo Fernandez Aguera <carferna@microsoft.com>
2021-05-13 20:00:32 -0700

    WIP: Automate descriptor writes.

--- engine/src/graphics/backend/DescriptorSet.h
+++ engine/src/graphics/backend/DescriptorSet.h
@@ -31,24 +31,24 @@ namespace rev::gfx
 		void addStorageBuffer(std::string name, uint32_t bindingPos, vk::ShaderStageFlags shaderStages)
 		{
 			m_bindings.emplace_back(bindingPos, vk::DescriptorType::eStorageBuffer, 1, shaderStages);
-			if (!name.empty())
-				m_namedBindings.insert({ name, bindingPos });
+			assert(!name.empty());
+			m_storageBufferBindings.insert({ name, bindingPos });
 			++m_numStorageBuffers;
 		}
 
 		void addTexture(std::string name, uint32_t bindingPos, vk::ShaderStageFlags shaderStages)
 		{
 			m_bindings.emplace_back(bindingPos, vk::DescriptorType::eCombinedImageSampler, 1, shaderStages);
-			if (!name.empty())
-				m_namedBindings.insert({ name, bindingPos });
+			assert(!name.empty());
+			m_textureBindings.insert({ name, bindingPos });
 			++m_numTextures;
 		}
 
 		void addTextureArray(std::string name, uint32_t bindingPos, uint32_t numTextures, vk::ShaderStageFlags shaderStages)
 		{
 			m_bindings.emplace_back(bindingPos, vk::DescriptorType::eCombinedImageSampler, numTextures, shaderStages);
-			if (!name.empty())
-				m_namedBindings.insert({ name, bindingPos });
+			assert(!name.empty());
+			m_textureArrayBindings.insert({ name, { bindingPos, numTextures } });
 			m_numTextures += numTextures;
 		}
 
@@ -71,6 +71,7 @@ namespace rev::gfx
 
 		auto layout() const { return m_vkLayout; }
 	private:
+		bool isStorageBuffer(std::string name, uint32_t& binding);
 
 		void createDescriptorPool(uint32_t numDescriptorSets)
 		{
@@ -100,17 +101,89 @@ namespace rev::gfx
 		}
 
 		std::vector<vk::DescriptorSetLayoutBinding> m_bindings;
-		std::map<std::string, uint32_t> m_namedBindings;
+		std::map<std::string, uint32_t> m_storageBufferBindings;
+		std::map<std::string, uint32_t> m_textureBindings;
+		std::map<std::string, std::pair<uint32_t,uint32_t>> m_textureArrayBindings;
 		uint32_t m_numTextures = 0;
 		uint32_t m_numStorageBuffers = 0;
 		vk::DescriptorSetLayout m_vkLayout;
 		vk::DescriptorPool m_pool;
 		std::vector<vk::DescriptorSet> m_descriptorSets;
+
+		friend class DescriptorSetUpdate;
 	};
 
-	struct DescriptorSetUpdate
+	class DescriptorSetUpdate
 	{
-		void add();
-		void send();
+	public:
+		DescriptorSetUpdate(DescriptorSetLayout& layout, uint32_t descNdx) : m_layout(layout), m_descNdx(descNdx) {}
+
+		void addStorageBuffer(const std::string& name, std::shared_ptr<GPUBuffer> buffer)
+		{
+			// Locate binding in layout
+			auto iter = m_layout.m_storageBufferBindings.find(name);
+			assert(iter != m_layout.m_storageBufferBindings.end());
+			
+			m_bufferWrites.insert({ iter->second, buffer });
+		}
+
+		void setTexture(const std::string& name, std::shared_ptr<Texture> texture)
+		{
+			// Locate binding in layout
+			auto iter = m_layout.m_textureBindings.find(name);
+			assert(iter != m_layout.m_textureBindings.end());
+
+			m_textureWrites.insert({ iter->second, texture });
+		}
+
+		void setTextureArray();
+
+		void send() const
+		{
+			std::vector<vk::WriteDescriptorSet> writes;
+
+			// Storage buffer writes
+			std::vector<vk::DescriptorBufferInfo> bufferInfo;
+			bufferInfo.reserve(m_bufferWrites.size());
+			for (auto& [bindingPos, buffer] : m_bufferWrites)
+			{
+				auto& writeBufferInfo = bufferInfo.emplace_back(buffer->buffer(), 0, buffer->size());
+
+				vk::WriteDescriptorSet& writeInfo = writes.emplace_back();
+				writeInfo.dstSet = m_layout.getDescriptor(m_descNdx);
+				writeInfo.descriptorType = vk::DescriptorType::eStorageBuffer;
+				writeInfo.dstBinding = bindingPos;
+				writeInfo.dstArrayElement = 0;
+				writeInfo.descriptorCount = 1;
+				writeInfo.pBufferInfo = &writeBufferInfo;
+			}
+
+			// Single texture writes
+			std::vector<vk::DescriptorImageInfo> textureInfo;
+			textureInfo.reserve(m_textureWrites.size());
+			for (auto& [bindingPos, texture] : m_textureWrites)
+			{
+				vk::DescriptorImageInfo& writeTextureInfo = textureInfo.emplace_back();
+				writeTextureInfo.imageLayout = vk::ImageLayout::eShaderReadOnlyOptimal;
+				writeTextureInfo.imageView = texture->image->view();
+				writeTextureInfo.sampler = texture->sampler;
+
+				vk::WriteDescriptorSet& writeInfo = writes.emplace_back();
+				writeInfo.dstSet = m_layout.getDescriptor(m_descNdx);
+				writeInfo.descriptorType = vk::DescriptorType::eCombinedImageSampler;
+				writeInfo.dstBinding = bindingPos;
+				writeInfo.dstArrayElement = 0;
+				writeInfo.descriptorCount = 1;
+				writeInfo.pImageInfo = &writeTextureInfo;
+			}
+
+			RenderContext().device().updateDescriptorSets(writes, {});
+		}
+
+	private:
+		DescriptorSetLayout& m_layout;
+		uint32_t m_descNdx;
+		std::map<uint32_t, std::shared_ptr<GPUBuffer>> m_bufferWrites;
+		std::map<uint32_t, std::shared_ptr<Texture>> m_textureWrites;
 	};
 }
